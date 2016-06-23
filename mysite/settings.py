587898309d5d82bfd20d1bb0f48a7724d7ef65da
asimonia <alex.simonian@gmail.com>
2016-06-23 17:30:51 -0500

    Finished urls and views

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'alex_schema',
         'USER': 'root',
-        'PASSWORD': 'password',
+        'PASSWORD': 'pass',
         'HOST': 'localhost',
         'PORT': '3306',
     }
