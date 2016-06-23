c6139ccdeb074d2aa6c61c0a9199769b8bc9e5a4
asimonia <alex.simonian@gmail.com>
2016-06-23 17:14:04 -0500

    Initial commit

---
+++ polls/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from django.utils import timezone
+import datetime
+
+
+# Create your models here.
+
+class Question(models.Model):
+    
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.question_text
+
+    def was_published_in_last_7_days(self):
+    	return self.pub_date >= timezone.now() - datetime.timedelta(days=7)
+
+    was_published_in_last_7_days.admin_order_field = 'pub_date'
+    was_published_in_last_7_days.boolean = True
+    was_published_in_last_7_days.short_description = 'Published This Week?'
+
+class Choice(models.Model):
+    
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    question = models.ForeignKey(Question)
+
+    def __unicode__(self):
+        return self.question_text
+    
